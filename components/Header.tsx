22be2fc55946505476c80d4e443018304eea2a3f
ABDULQUADIRL <elanquad92@gmail.com>
2025-05-30 05:12:42 +0100

    modify Footer
    Modify Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,13 @@
 export default function Header() {
     return (
-        <header className="bg-blue-100  p-4 shadow">
+        <header className="bg-pink-800  p-4 shadow">
             <div className="max-w-6xl mx-auto flex justify-between items-center">
-                <h1 className="text-2xl font-bold text-blue-800">
+                <h1 className="text-2xl font-bold text-white">
                     Nirvana De Spa
                 </h1>
                 <nav className="space-x-4">
-                    <a href="/" className="mr-4 hover:text-blue-800">Home</a>
-                    <a href="/booking" className="text-purple-600 hover:text-purple-800">Book a Session</a>
+                    <a href="/" className="mr-4 hover:text-white">Home</a>
+                    <a href="/booking" className="text-white hover:text-pink-400">Book a Session</a>
                 </nav>
             </div>
         </header>
